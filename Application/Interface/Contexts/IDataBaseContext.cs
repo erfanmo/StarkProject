cf4f1228a020eb3a1f671c20ed6150cabf130084
Erfan Mohebi <mohammadreza.moebi@edag.com>
2023-05-22 20:06:39 +0200

    Add project files.

---
+++ Application/Interface/Contexts/IDataBaseContext.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Application.Interface.Contexts
+{
+    public interface IDataBaseContext
+    {
+        DbSet<User> Users { get; set; }
+        int SaveChanges();
+        int SaveChanges(bool acceptAllChangesOnSuccess);
+        Task<int> SaveChangesAsync(bool acceptAllChangesOnSuccess, CancellationToken cancellationToken = default);
+        Task<int> SaveChangesAsync(CancellationToken cancellationToken = default);
+    }
+}
---
+++ Persistence/Contexts/DataBaseContext.cs
@@ -0,0 +1,69 @@
+﻿using Application.Interface.Contexts;
+using Domain.Attribute;
+using Domain.Users;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Persistence.Contexts
+{
+    public class DataBaseContext :DbContext,IDataBaseContext
+    {
+        public DataBaseContext(DbContextOptions options) :base(options)
+        {
+
+        }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            //modelBuilder.Entity<User>().Property<DateTime?>("InsertTime");
+            //modelBuilder.Entity<User>().Property<DateTime?>("UpdateTime");
+            foreach (var item in modelBuilder.Model.GetEntityTypes())
+            {
+                if (item.ClrType.GetCustomAttributes(typeof(AuditableAttribute),true).Length > 0)
+                {
+                    modelBuilder.Entity(item.Name).Property<DateTime>("InsertTime");
+                    modelBuilder.Entity(item.Name).Property<DateTime?>("UpdateTime");
+                    modelBuilder.Entity(item.Name).Property<DateTime?>("RemovedTime");
+                    modelBuilder.Entity(item.Name).Property<bool?>("IsRemoved");
+                }
+            }
+            base.OnModelCreating(modelBuilder);
+        }
+
+        public override int SaveChanges()
+        {
+            var modifiedEntries = ChangeTracker.Entries()
+                .Where(p=>p.State == EntityState.Modified ||
+                 p.State == EntityState.Added || p.State == EntityState.Deleted);
+            foreach (var item in modifiedEntries)
+            {
+                var entityType = item.Context.Model.FindEntityType(item.Entity.GetType());
+                var inserted = entityType.FindProperty("InsertTime");
+                var update = entityType.FindProperty("UpdateTime");
+                var remove = entityType.FindProperty("RemoveTime");
+                var Isremove = entityType.FindProperty("IsRemove");
+
+                if(item.State == EntityState.Added && inserted != null) 
+                {
+                    item.Property("InserteTime").CurrentValue = DateTime.Now;
+                }
+                if (item.State == EntityState.Modified && update != null)
+                {
+                    item.Property("UpdateTIme").CurrentValue = DateTime.Now;
+                }
+                if (item.State == EntityState.Deleted && remove != null && Isremove != null)
+                {
+                    item.Property("RemoveTime").CurrentValue = DateTime.Now;
+                    item.Property("IsRemove").CurrentValue = true;
+                }
+            }
+            return base.SaveChanges();
+        }
+    }
+}
